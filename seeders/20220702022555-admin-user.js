b2cb4fff77123b6d142ef0929e1186df5c59eb09
hamamullaiqi <hamamullaiqi@gmail.com>
2023-07-18 08:21:32 +0700

    fixing level seeder user

--- seeders/20220702022555-admin-user.js
+++ seeders/20220702022555-admin-user.js
@@ -1,37 +1,37 @@
-'use strict';
-const  bcrypt = require("bcrypt");
-const uuid = require('uuid') 
+"use strict";
+const bcrypt = require("bcrypt");
+const uuid = require("uuid");
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-     
+    async up(queryInterface, Sequelize) {
+        /**
+         * Add seed commands here.
+         *
+         * Example:
+         * await queryInterface.bulkInsert('People', [{
+         *   name: 'John Doe',
+         *   isBetaMember: false
+         * }], {});
+         */
 
-    await queryInterface.bulkInsert("users", [{
-      id: uuid.v4(),
-      username: "admin",
-      email: "admin@admin.com",
-      full_name:"admin",
-      password: bcrypt.hashSync("123456", bcrypt.genSaltSync(10)),
-      level: 10,
-    }])
-  },
+        await queryInterface.bulkInsert("users", [
+            {
+                id: uuid.v4(),
+                username: "admin",
+                email: "admin@admin.com",
+                full_name: "admin",
+                password: bcrypt.hashSync("123456", bcrypt.genSaltSync(10)),
+                level: 0x1fff0,
+            },
+        ]);
+    },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-      await queryInterface.bulkDelete('users', null, {});
-
-  }
+    async down(queryInterface, Sequelize) {
+        /**
+         * Add commands to revert seed here.
+         *
+         * Example:
+         * await queryInterface.bulkDelete('People', null, {});
+         */
+        await queryInterface.bulkDelete("users", null, {});
+    },
 };
