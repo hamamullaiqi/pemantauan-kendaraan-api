c18383cd1db93345d0af8c2c2d1b0426c568fdb0
andi lauw <andi.safariansyah7@gmail.com>
2023-07-27 00:14:47 +0700

    .

--- src/controllers/vendor.js
+++ src/controllers/vendor.js
@@ -3,12 +3,12 @@ import getValidateInputVendor from "../services/getValidateInputVendor";
 const { vendor } = require("../../models");
 
 const vendorCtrl = createCrud({
-    models: vendor,
-    option: (req, res) => ({
-        attributes: {
-            exclude: ["createdAt", "updatedAt", "id"],
-        },
-    }),
-    onBeforeSave: getValidateInputVendor,
+  models: vendor,
+  option: (req, res) => ({
+    attributes: {
+      exclude: ["createdAt", "updatedAt", "id"],
+    },
+  }),
+  onBeforeSave: getValidateInputVendor,
 });
 export default vendorCtrl;
